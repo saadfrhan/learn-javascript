e026d908e2aaaaaee0ff185576d6eca0a46378ee
saadfrhan <saadfrhan@users.noreply.github.com>
2024-02-23 06:51:10 +0000

    deploy: 2f293554afd54d9151b148cde7a803c69d9d59ca

---
+++ 1-where-to-run/1.1-node.js
@@ -0,0 +1,21 @@
+console.log("Hello World!");
+
+// Install NVM
+
+// For Linux/Mac
+// https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating
+// For Windows
+// https://github.com/coreybutler/nvm-windows/releases
+
+// Open terminal
+// run:
+// nvm install node
+// then nvm will install the latest version of node
+// and set it as selected version
+// now run:
+// node -v
+// it hopefully shows you the latest version of node
+// now pop into this folder, open terminal and run
+// node 1.1-node.js
+// hopefully it will output Hello World in terminal
+// after the line of the ran command.
---
+++ 1-where-to-run/1.4-external/js/index2.js
@@ -0,0 +1,1 @@
+console.log("I am from /js/index2.js");
