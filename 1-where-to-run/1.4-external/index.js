26a89017d74f40f87bec8dc32892cfcad255211d
saadfrhan <saadfarhan347@gmail.com>
2024-02-23 11:35:06 +0500

    add 1

---
+++ 1-where-to-run/1.4-external/index.js
@@ -0,0 +1,5 @@
+// This DOM API gets access to the HTML elements of the HTML file,
+// in which this .js file is linked in.
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
